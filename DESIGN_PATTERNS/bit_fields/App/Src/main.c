9d7c91e936a92ad23df9b787ae93ecd9282de8f3
slemanz <william_sleman@hotmail.com>
2025-06-22 18:48:56 -0300

    bit fields(feat): add example of use

--- DESIGN_PATTERNS/bit_fields/App/Src/main.c
+++ DESIGN_PATTERNS/bit_fields/App/Src/main.c
@@ -2,11 +2,33 @@
 #include "config.h"
 #include <stdio.h>
 
+typedef struct
+{
+    unsigned enable : 1;
+    unsigned errorStatus : 1;
+    unsigned sensorResolution : 4;
+    unsigned currentState : 2;
+    unsigned nextState : 2;
+}statusBits_t;
+
+statusBits_t device_status;
+
 int main(void)
 {
     config_drivers();
     printf("Init\n");
-    //test_setup();
+    
+    device_status.enable = 1;
+    device_status.errorStatus = 0;
+    device_status.sensorResolution = 3;
+
+    if(device_status.enable)
+    {
+        printf("Device enabled\n");
+    }else
+    {
+        printf("Device is currenctly disabled\n");
+    }
 
 
     while(1)
