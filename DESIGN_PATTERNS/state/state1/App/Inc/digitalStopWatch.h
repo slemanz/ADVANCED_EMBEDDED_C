ca90a3ecdd4fb1e852a2794f4cf704566bbc3c07
slemanz <william_sleman@hotmail.com>
2025-06-23 14:08:35 -0300

    state(feat): implement create and start watch

--- DESIGN_PATTERNS/state/state1/App/Inc/digitalStopWatch.h
+++ DESIGN_PATTERNS/state/state1/App/Inc/digitalStopWatch.h
@@ -1,6 +1,13 @@
 #ifndef INC_DIGITALSTOPWATCH_H_
 #define INC_DIGITALSTOPWATCH_H_
 
+#include "timeSource.h"
+
+typedef struct digitalStopWatch *digitalStopWatchPtr;
+
+digitalStopWatchPtr watch_create(const char *name, const TimeSource *tmr);
+void watch_start(digitalStopWatchPtr instance, long max_time, long *current_count);
+void watch_stop(digitalStopWatchPtr instance);
 
 
 #endif /* INC_DIGITALSTOPWATCH_H_ */
--- DESIGN_PATTERNS/state/state1/App/Src/digitalStopWatch.c
+++ DESIGN_PATTERNS/state/state1/App/Src/digitalStopWatch.c
@@ -1,6 +1,9 @@
 #include "digitalStopWatch.h"
 #include "timeSource.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef enum{
     stopped,
     started
@@ -11,3 +14,57 @@ struct digitalStopWatch{
     TimeSource source;
     const char *name;
 };
+
+digitalStopWatchPtr watch_create(const char *name, const TimeSource *tmr)
+{
+    digitalStopWatchPtr new_watch = (digitalStopWatchPtr)calloc(sizeof(digitalStopWatchPtr), sizeof(digitalStopWatchPtr));
+    if(new_watch)
+    {
+        new_watch->state = stopped;
+        new_watch->name = name;
+        new_watch->source.timer = tmr->timer;
+        printf("Watch created succesfully\n");
+    }
+    else
+    {
+        printf("Low memory. Could not create watch.\n");
+    }
+
+    return new_watch;
+}
+
+void watch_start(digitalStopWatchPtr instance, long max_time, long *current_count)
+{
+    switch (instance->state)
+    {
+        case started:
+            printf("Watch has already started\n");
+            break;
+        
+        case stopped:
+            instance->state = started;
+            printf("Started\n");
+            switch (instance->source.timer)
+            {
+                case TIMER2:
+                    timer2_sec_set(max_time, current_count);
+                    break;
+                case TIMER_SYSTICK:
+                    systick_millis_set(max_time, current_count);
+                    break;
+                
+                default:
+                    break;
+                
+            }
+            break;
+        default:
+            printf("State does not exist.\n");
+            break;
+    }
+}
+
+void watch_stop(digitalStopWatchPtr instance)
+{
+
+}
