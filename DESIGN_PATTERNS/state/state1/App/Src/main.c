0d182b2155d5424efb9c1baa84713e30899971cd
slemanz <william_sleman@hotmail.com>
2025-06-23 11:03:04 -0300

    state(feat): implement timer systick source

--- DESIGN_PATTERNS/state/state1/App/Src/main.c
+++ DESIGN_PATTERNS/state/state1/App/Src/main.c
@@ -9,11 +9,11 @@ int main(void)
     config_drivers();
     printf("Init\n");
 
-    long seconds = 0;
+    long milliseconds = 0;
     
     while(1)
     {
-        timer2_sec_set(2, &seconds);
+        systick_millis_set(2500, &milliseconds);
         GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
     }
 }
--- DESIGN_PATTERNS/state/state1/App/Src/timeSource.c
+++ DESIGN_PATTERNS/state/state1/App/Src/timeSource.c
@@ -28,7 +28,7 @@ void timer2_sec_set(int max_sec, long *current_count)
         seconds++;
         *current_count = seconds;
         printf("Elapsed time: %ld\n", seconds);
-        
+
         // clear UIF
         TIM2->SR &= ~(1U << 0);
     }
@@ -40,5 +40,29 @@ void timer2_sec_reset(void)
     TIM2->CR1 = 0;
 }
 
-void systick_millis_set(int max_millis, long *current_count);
-void systick_millis_reset(void);
+
+// 16 000 000 = 1 sec
+// 16 000 000/1 000 = 16 000
+void systick_millis_set(int max_millis, long *current_count)
+{
+    SYSTICK->LOAD = 16000-1; /* Reload with number per seconds */
+    SYSTICK->VAL = 0;
+
+    SYSTICK->CTRL = 5; /* Enable systick, No interrupt, use system clock */
+
+    long milliseconds = 0;
+    for(int i = 0; i < max_millis; i++)
+    {
+        while(!(SYSTICK->CTRL & (1U << 16))); /* Count flag */
+        milliseconds++;
+        *current_count = milliseconds;
+    }
+
+    printf("Elapsed time %ld\n\r", milliseconds);
+    SYSTICK->CTRL = 0; /* Disable clock */
+}
+
+void systick_millis_reset(void)
+{
+    SYSTICK->CTRL = 0; /* Disable clock */
+}
