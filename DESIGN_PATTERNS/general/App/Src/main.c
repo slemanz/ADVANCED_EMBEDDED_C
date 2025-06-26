7b3577f12c9848ef7643c781b2df32e557b770c1
slemanz <william_sleman@hotmail.com>
2025-06-26 10:28:25 -0300

    general(feat): implement btn update function

--- DESIGN_PATTERNS/general/App/Src/main.c
+++ DESIGN_PATTERNS/general/App/Src/main.c
@@ -2,6 +2,7 @@
 #include "config.h"
 #include <stdio.h>
 
+void btn_update(void);
 
 int main(void)
 {
@@ -10,14 +11,31 @@ int main(void)
 
     uint64_t start_time = ticks_get();
 
-
     while(1)
     {
-        if((ticks_get() - start_time) >= 200)
+        btn_update();
+
+        if((ticks_get() - start_time) >= 10000)
         {
-            GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+            printf("Working\n");
             start_time = ticks_get();
         }
+    }
+}
+
+uint8_t btnState = 0, btnStateOld = 0;
+
+void btn_update(void)
+{
+    btnState = GPIO_ReadFromInputPin(GPIOC, GPIO_PIN_NO_13);
+    if(btnState && (btnStateOld != btnState))
+    {
+        GPIO_WriteToOutputPin(GPIOA, GPIO_PIN_NO_5, GPIO_PIN_RESET);
+        btnStateOld = btnState;
+    }else if(btnStateOld != btnState)
+    {
+        GPIO_WriteToOutputPin(GPIOA, GPIO_PIN_NO_5, GPIO_PIN_SET);
+        btnStateOld = btnState;
     }
 }
 
