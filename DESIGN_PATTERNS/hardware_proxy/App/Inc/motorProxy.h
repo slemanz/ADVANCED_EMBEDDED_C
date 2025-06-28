6402d2d9f18e6ee008edef60443dad47a8afaf4d
slemanz <william_sleman@hotmail.com>
2025-06-28 15:55:11 -0300

    hw proxy(feat): add motorProxy prototypes

--- DESIGN_PATTERNS/hardware_proxy/App/Inc/motorProxy.h
+++ DESIGN_PATTERNS/hardware_proxy/App/Inc/motorProxy.h
@@ -1,6 +1,33 @@
 #ifndef INC_MOTOR_PROXY_H_
 #define INC_MOTOR_PROXY_H_
 
+#include "motorData.h"
+#include <stdlib.h>
+#include <stdint.h>
+
+struct motorProxy{
+    uint32_t *motorData;
+    volatile uint32_t *motorAddr;
+    uint32_t rotaryArmLength;
+    const char *name;
+};
+
+typedef struct motorProxy motorProxy;
+
+motorProxy* motorProxy_create(const char *name);
+void motorProxy_destroy(motorProxy *const me);
+
+void motorProxy_init(motorProxy *const me, const char *name);
+void motorProxy_cleanUp(motorProxy *const me);
+void motorProxy_enable(motorProxy *const me);
+void motorProxy_disable(motorProxy *const me);
+void motorProxy_configure(motorProxy *const me, uint32_t length, volatile uint32_t *location, int32_t *motorData);
+
+Direction_e motorProxy_accessMotorDirection(motorProxy *const me);
+uint32_t motorProxy_accessMotorSpeed(motorProxy *const me);
+uint32_t motorProxy_accessMotorState(motorProxy *const me);
+void motorProxy_writeMotorSpeed(motorProxy *const me, Direction_e direction, uint32_t speed);
+void motorProxy_clearErrorStatus(motorProxy *const me);
 
 
 #endif /* INC_MOTOR_PROXY_H_ */
---
+++ DESIGN_PATTERNS/hardware_proxy/App/Src/motorProxy.c
@@ -0,0 +1,1 @@
+#include "motorProxy.h"
