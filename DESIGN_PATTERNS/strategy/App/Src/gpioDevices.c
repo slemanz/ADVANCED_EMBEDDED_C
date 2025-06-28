81a3ed90c0682cf531579b3b3212c86393badda0
slemanz <william_sleman@hotmail.com>
2025-06-27 23:21:14 -0300

    strategy(feat): add gpioDevices source file

---
+++ DESIGN_PATTERNS/strategy/App/Src/gpioDevices.c
@@ -0,0 +1,17 @@
+#include "gpioDevices.h"
+#include <stdio.h>
+
+void gpioADeviceStrategy(int device_number)
+{
+
+}
+
+void gpioBDeviceStrategy(int device_number)
+{
+
+}
+
+void gpioCDeviceStrategy(int device_number)
+{
+
+}
