c5acc5bdbee8b1fe13474182937a90d66b273197
slemanz <william_sleman@hotmail.com>
2025-06-28 18:13:32 -0300

    hw proxy(feat): implement marshal funtion

--- DESIGN_PATTERNS/hardware_proxy/App/Src/motorProxy.c
+++ DESIGN_PATTERNS/hardware_proxy/App/Src/motorProxy.c
@@ -1,6 +1,8 @@
 #include "motorProxy.h"
 #include <stdio.h>
 
+static uint32_t marshal(const motorData mData);
+
 motorProxy* motorProxy_create(const char *name)
 {
     motorProxy *me = (motorProxy *)malloc(sizeof(motorProxy));
@@ -69,4 +71,32 @@ void motorProxy_configure(motorProxy *const me, uint32_t length, volatile uint32
 Direction_e motorProxy_accessMotorDirection(motorProxy *const me)
 {
     return NO_DIRECTION;
+}
+
+static uint32_t marshal(const motorData mData)
+{
+    uint32_t deviceCmd = 0;
+
+    if(mData.on_off) deviceCmd |= (1 << 0);
+
+    if(mData.direction == FORWARD)
+    {
+        deviceCmd |= (2 << 1);
+    }else if(mData.direction == REVERSE)
+    {
+        deviceCmd |= (1 << 1);
+    }
+
+    if(mData.speed < 32) deviceCmd |= (mData.speed << 3); // 5 bits
+
+    if(mData.errorStatus)           deviceCmd |= (1 << 8);
+    if(mData.noPowerError)          deviceCmd |= (1 << 9);
+    if(mData.noTorqueError)         deviceCmd |= (1 << 10);
+    if(mData.BITError)              deviceCmd |= (1 << 11);
+    if(mData.overTemperatureError)  deviceCmd |= (1 << 12);
+    if(mData.reserverdError1)       deviceCmd |= (1 << 13);
+    if(mData.reserverdError2)       deviceCmd |= (1 << 14);
+    if(mData.unknownError)          deviceCmd |= (1 << 15);
+
+    return deviceCmd;
 }
