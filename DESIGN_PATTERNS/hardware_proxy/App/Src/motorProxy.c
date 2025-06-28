19dbfa44689f51b2281ac64df1066f41757c418f
slemanz <william_sleman@hotmail.com>
2025-06-28 16:47:24 -0300

    hw proxy(feat): begin to implement functions

--- DESIGN_PATTERNS/hardware_proxy/App/Src/motorProxy.c
+++ DESIGN_PATTERNS/hardware_proxy/App/Src/motorProxy.c
@@ -1,1 +1,58 @@
 #include "motorProxy.h"
+#include <stdio.h>
+
+motorProxy* motorProxy_create(const char *name)
+{
+    motorProxy *me = (motorProxy *)malloc(sizeof(motorProxy));
+    if(me)
+    {
+        motorProxy_init(me, name);
+    }
+
+    return me;
+}
+
+void motorProxy_destroy(motorProxy *const me)
+{
+    if(me)
+    {
+        motorProxy_cleanUp(me);
+    }
+
+    free(me);
+}
+
+void motorProxy_init(motorProxy *const me, const char *name)
+{
+    me->motorAddr = NULL;
+    me->name = name;
+}
+
+void motorProxy_cleanUp(motorProxy *const me)
+{
+
+}
+
+void motorProxy_enable(motorProxy *const me)
+{
+    if(!me->motorData)
+    {
+        return;
+    }
+
+    (*(volatile uint32_t *)(*me->motorAddr)) |= 1;
+
+    printf("%s, %d, %p\n", __func__, *me->motorAddr, me->motorAddr);
+}
+
+void motorProxy_disable(motorProxy *const me)
+{
+    if(!me->motorData)
+    {
+        return;
+    }
+
+    (*(volatile uint32_t *)(*me->motorAddr)) &= ~1;
+
+    printf("%s, %d, %p\n", __func__, *me->motorAddr, me->motorAddr);
+}
