13a90a34b8b32971723bb3eaecbf2760495c921e
slemanz <william_sleman@hotmail.com>
2025-07-05 22:05:24 -0300

    publisher subscriber(feat): add feertilizer mixer client interface file

---
+++ DESIGN_PATTERNS/publisher_subscriber/App/Inc/fertilizer_mixer_client.h
@@ -0,0 +1,23 @@
+#ifndef INC_FERTILIZER_MIXER_CLIENT_H_
+#define INC_FERTILIZER_MIXER_CLIENT_H_
+
+#include "env_sensor.h"
+#include "enviromental_data.h"
+
+typedef struct fertilizer_mixer_client
+{
+    env_data_t data;
+    callback_fn fn_handler;
+}fertilizer_mixer_client_t;
+
+fertilizer_mixer_client_t *fertilizer_mixer_client_create(void);
+void fertilizer_mixer_client_init(fertilizer_mixer_client_t *const me);
+
+void fertilizer_mixer_client_handleNewData(fertilizer_mixer_client_t *const me);
+void fertilizer_mixer_client_subscribe(fertilizer_mixer_client_t *const me, env_sensor_t *const sensor);
+void fertilizer_mixer_client_unsubscribe(fertilizer_mixer_client_t *const me, env_sensor_t *const sensor);
+
+void fertilizer_mixer_client_destroy(fertilizer_mixer_client_t *const me);
+
+
+#endif /* INC_FERTILIZER_MIXER_CLIENT_H_ */
