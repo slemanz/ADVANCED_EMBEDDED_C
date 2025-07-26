215c033ebdcbf172d4ae9b0f5c88aec10f2026dd
slemanz <william_sleman@hotmail.com>
2025-07-26 20:11:39 -0300

    semihosting(feat): add use example of semihosting

--- BUILD_SYSTEMS/semihosting/README.md
+++ BUILD_SYSTEMS/semihosting/README.md
@@ -7,3 +7,12 @@ embedded code, running on a target device, to utilize the input/output
 Essentially, it enables functions like printf and scanf in your embedded
 code to work with the host's console and file system instead of requiring
 dedicated I/O peripherals on the target
+
+### Example
+
+Here, the most simple implementation:
+
+- [Linker](Src/linker.ld)
+- [Startup](Src/startup.c)
+- [main.c](Src/main.c)
+- [Makefile](Makefile)
--- BUILD_SYSTEMS/semihosting/Src/main.c
+++ BUILD_SYSTEMS/semihosting/Src/main.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
-#include <adc.h>
+#include <stdint.h>
+
+#define RCC_BASE        0x40023800
+#define GPIOA_BASE      0x40020000
+
+#define RCC_AHB1ENR     (*(volatile unsigned int *)(RCC_BASE + 0x30))
+#define GPIOA_MODER     (*(volatile unsigned int *)(GPIOA_BASE + 0x00))
+#define GPIOA_ODR       (*(volatile unsigned int *)(GPIOA_BASE + 0x14))
+
+//uint32_t sensor_value;
+
+extern void initialise_monitor_handles(void);
 
 int main(void)
 {
+    initialise_monitor_handles();
 
-    while (1) {
+    RCC_AHB1ENR |= (1 << 0);
+    GPIOA_MODER |= (1 << 10);
+    GPIOA_MODER &= ~(1 << 11);
 
-        for(unsigned int i = 0; i <= 200000; i++);;
+    printf("Init board!\n");
+
+    uint32_t data = 0;
+
+    while (1)
+    {
+        printf("Toggle time: %d\n", (int)data);
+        data++;
+        GPIOA_ODR ^= (1 << 5);
+        for(unsigned int i = 0; i <= 800000; i++);
     }
 }
