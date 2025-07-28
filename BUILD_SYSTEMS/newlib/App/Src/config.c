9103cdc3efe3f9cb15983b31748f2774ba1e16aa
slemanz <william_sleman@hotmail.com>
2025-07-28 05:34:57 -0300

    newlib(feat): add main application

---
+++ BUILD_SYSTEMS/newlib/App/Src/config.c
@@ -0,0 +1,23 @@
+#include "config.h"
+#include "driver_gpio.h"
+#include "driver_fpu.h"
+
+void gpio_setup(void)
+{
+    GPIO_Handle_t GpioLed;
+	GpioLed.pGPIOx = GPIOA;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_LOW;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+    GPIO_Init(&GpioLed);
+}
+
+void config_drivers(void)
+{
+    //fpu_enable();
+    gpio_setup();
+
+}
--- BUILD_SYSTEMS/newlib/App/Src/main.c
+++ BUILD_SYSTEMS/newlib/App/Src/main.c
@@ -1,10 +1,14 @@
-#include "common_defines.h"
 #include <stdio.h>
+#include "config.h"
+#include "driver_gpio.h"
 
 int main(void)
 {
+    config_drivers();
+
     while(1)
     {
-
+        GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+        for(uint32_t i = 0; i < 1000000; i++);
     }
 }
