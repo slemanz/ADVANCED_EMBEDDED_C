bc5acd8ff24eb43ce773c3da30a41ffcc8b364e7
slemanz <william_sleman@hotmail.com>
2025-08-23 09:55:11 -0300

    tdd(feat): add makefile and first files to starting

---
+++ TDD/starting/tests/LedDriverTests.c
@@ -0,0 +1,21 @@
+#include "unity_fixture.h"
+
+TEST_GROUP(LedDriver);
+
+TEST_SETUP(LedDriver)
+{
+}
+
+TEST_TEAR_DOWN(LedDriver)
+{
+}
+
+TEST(LedDriver, LedsOffAfterCreate)
+{
+    TEST_FAIL_MESSAGE("Start here");
+}
+
+TEST_GROUP_RUNNER(LedDriver)
+{
+    RUN_TEST_CASE(LedDriver, LedsOffAfterCreate);
+}
