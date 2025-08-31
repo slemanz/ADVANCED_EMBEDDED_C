e091e8794c34b6bcad6291b961940438ec470010
slemanz <william_sleman@hotmail.com>
2025-08-31 07:25:09 -0300

    tdd(feat): begin scheduler tests

---
+++ TDD/production_code/tests/LightSchedulerTest.cpp
@@ -0,0 +1,12 @@
+#include "CppUTest/TestHarness.h"
+
+TEST(LightScheduler, ScheduleOnEverydayNotTimeYet)
+{
+    LightScheduler_ScheduleTurnOn(3, EVERYDAY, 1200);
+    FakeTimeService_SetDay(MONDAY);
+    FakeTimeService_SetMinute(1199);
+    LightScheduler_Wakeup();
+
+    LONGS_EQUAL(LIGHT_ID_UNKNOWN, LightControllerSpy_GetLastId());
+    LONGS_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_GetLastState());
+}
