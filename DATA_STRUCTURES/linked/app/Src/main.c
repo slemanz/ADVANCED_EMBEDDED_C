b9441bfb492eda9232395489b8f3b10695531c2d
slemanz <william_sleman@hotmail.com>
2025-09-23 04:19:07 -0300

    linked(feat): add driver interrupt uart

--- DATA_STRUCTURES/linked/app/Src/main.c
+++ DATA_STRUCTURES/linked/app/Src/main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include "config.h"
-#include "led.h"
-#include "button.h"
 #include "driver_systick.h"
-#include "driver_adc.h"
+#include "driver_uart.h"
 
 int main(void)
  {
@@ -12,31 +10,20 @@ int main(void)
 
     printf("\nInit board...\n\r");
 
-    uint32_t adc_value = 0;
-
-    uint64_t start_time = ticks_get();
+    //uint64_t start_time = ticks_get();
 
     while (1)
     {   
-        // blinky
-        if((ticks_get() - start_time) >= 500)
-        {
-            led_toggle();
-            start_time = ticks_get();
-        }
+    }
+}
 
-        // blinky 2
-        /*
-        GPIO_ToggleOutputPin(LED_PORT, LED_PIN);
-        ticks_delay(500);
-        */
+void USART2_IRQHandler(void)
+{
+    uint8_t ch;
 
-        if(!button_get_state())
-        {
-            adc_start_conversion();
-            adc_value = adc_read();
-            printf("Adc: %ld\n", adc_value);
-            while(!button_get_state());
-        }
+    if(UART2->SR & UART_SR_RXNE)
+    {
+        ch = UART2->DR;
+        uart2_write_byte(ch);
     }
 }
--- DATA_STRUCTURES/linked/drivers/Inc/driver_uart.h
+++ DATA_STRUCTURES/linked/drivers/Inc/driver_uart.h
@@ -3,9 +3,13 @@
 
 #include "stm32f411xx.h"
 
-#define UART_CR1_TE					(1U<<3)
-#define UART_CR1_UE					(1U<<13)
-#define UART_SR_TXE					(1U<<7)
+#define UART_CR1_TE					(1U << 3)
+#define UART_CR1_RE					(1U << 2)
+#define UART_CR1_UE					(1U << 13)
+#define UART_CR1_RXNEIE             (1U << 5)
+
+#define UART_SR_TXE					(1U << 7)
+#define UART_SR_RXNE                (1U << 5)
 
 void uart2_init(void);
 void uart2_init_pins(void);
