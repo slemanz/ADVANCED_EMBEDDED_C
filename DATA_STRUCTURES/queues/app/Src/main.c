5078a50563b63dfdf7a807c0da78e2bd4d61a7f2
slemanz <william_sleman@hotmail.com>
2025-09-27 14:50:54 -0300

    queues(feat): add queue structure

--- DATA_STRUCTURES/queues/app/Src/main.c
+++ DATA_STRUCTURES/queues/app/Src/main.c
@@ -7,6 +7,19 @@
 #include "driver_uart.h"
 #include "driver_rtc.h"
 
+#define QUEUE_SIZE     32
+
+/* Structre to represent the queue */
+typedef struct {
+    uint8_t buffer[QUEUE_SIZE]; // Fixed-size buffer for queue storage
+    uint8_t front;              // Index of the first element
+    uint8_t rear;               // Index of the next insertion point
+    uint8_t length;             // Number of elements currently in the queue
+} Queue_t;
+
+/* Queue instance for the UART RX buffer */
+static Queue_t uart_rx_queue = {.front = 0, .rear = 0, .length = 0};
+
 int main(void)
  {
     config_drivers();
